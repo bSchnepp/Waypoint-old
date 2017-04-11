a0891e3b182d7cc5d0b6aa538f8d8ed986553938
Brian <bschneppdev@gmail.com>
2017-04-10 17:17:49 -0700

    Started rewrite.

---
+++ include/waypoint.h
@@ -0,0 +1,47 @@
+/*
+Copyright (c) 2017 Brian Schnepp
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+
+#ifndef __WAYPOINT__H__
+#define __WAYPOINT__H__
+
+
+#ifndef WAYPOINT_LEAN_AND_MEAN
+		//Bloaty stuff we'll generally not need.
+#endif
+
+// This is the header for our userspace processes...
+#include <string.h>
+
+//Bind this into kernel later...
+
+
+
+WayProcess* SpawnProcess(wchar_t* name);	//TODO for this... Child process is always dependant on parent, yada yada.
+WayProcess* SpawnProcessAs(wchar_t* name, WayUser user);	//usually we'll have the user account created at install time, the Administrator permission group (which is a WayUser), and the user called WAY_SYSTEM, which is like 'root' on a UNIX.
+
+void RetrieveDirectory(char* buffer, size_t size);
+void CreateDirectory(char* parent, char* name, int permissions);	//We'll need to fix FreedomCore to adapt to this.
+
+
+
+#endif
---
+++ src/Boot/EFI/efi_main.c
@@ -0,0 +1,12 @@
+//TODO, decide between Intel or GNU, will probably go with Intel because it's reference and all.
+//Hmmmmmmmmmmmmmmmm
+
+//Written by Brian Schenpp, under the MIT license, TODO copy paste license in here, I'm lazy.
+
+#define mem_sz_proper_unit long long int
+
+struct kernel_position
+{
+	mem_sz_proper_unit* kernel_head;
+	//TODO
+}kernel_position;
