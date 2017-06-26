acbd3fd2778c2b03ee4735c21a05b16721c42f6b
Brian Schnepp <bschneppdev@gmail.com>
2017-06-26 15:19:49 -0700

    Completely re-doing UEFI support.

--- Feral/efistub/libre-efi/deps/archdeps.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-
-##I'll assume you at least have sudo installed.
-
-sudo pacman -S bmake
--- Feral/efistub/libre-efi/deps/debdeps.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-
-##I don't run Debian unstable anymore, no clue if this works or not.
-sudo apt install bmake
